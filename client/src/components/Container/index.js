14337aaad49a2068de65791d06b0c22af480fd53
Ed Courtney <edcourtney74@gmail.com>
2019-01-19 10:09:00 -0500

    Added Router, created pages

--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./style.css";
 
 function Container() {
-<div className="container">
-</div>
+    return (
+        <div className="container">
+        </div>
+    )
 }
 
 export default Container
