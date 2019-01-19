883ecbcb7358f5a37ce5935161db3ff74aabd8a2
Ed Courtney <edcourtney74@gmail.com>
2019-01-19 13:47:27 -0500

    Initial design for home page sidebar

---
+++ client/src/components/SidebarCard/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.css";
+
+function SidebarCard({ children }) {
+    return (
+        <div>
+        <div className="card m-2">
+            <img className="card-img-top" src="../images/Bairon2.jpg" alt="Child" />
+                <div className="card-body p-1">
+                    <h6><strong>Bairon Duban</strong></h6>
+                    <div class="small"><i class="fas fa-birthday-cake mr-2"></i>4/23/03<br />
+                    <i class="fas fa-globe-americas mr-2"></i>Choluteca, Honduras</div>
+                </div>
+        </div>
+        <div className="card m-2">
+            <img className="card-img-top" src="../images/Solanyi.jpg" alt="Child" />
+                <div className="card-body p-1">
+                    <h6><strong>Solanyi Nicol</strong></h6>
+                    <div class="small"><i class="fas fa-birthday-cake mr-2"></i>2/17/10<br />
+                    <i class="fas fa-globe-americas mr-2"></i>Managua, Nicaragua</div>
+                </div>
+        </div>  
+        </div>      
+    )
+}
+                    
+export default SidebarCard
+
