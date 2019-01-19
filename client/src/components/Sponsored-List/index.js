8291ba9857f018d56f158265d4b83d4cf3819ddd
nosidam48 <madison.tift68@gmail.com>
2019-01-18 20:18:48 -0500

    added basic donor page and navbar

--- client/src/components/Sponsored-List/index.js
+++ client/src/components/Sponsored-List/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./style.css";
 
 function SponsoredList() {
-
+return(
+<div className="col-2">
+<h5>Your kids: </h5>
+</div>
+)
 }
 
 export default SponsoredList
--- client/src/components/Sponsored-Photos/index.js
+++ client/src/components/Sponsored-Photos/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "./style.css";
+import ImageGallery from 'react-image-gallery';
 
-function SponsoredPhotos() {
+class SponsoredPhotos extends React.Component {
+ 
+  render() {
 
+ 
+    const images = [
+      {
+        original: "../images/Carlitos.jpg",
+        thumbnail: '../images/Carlitos.jpg',
+      },
+      {
+        original: '../images/Carlitos2.jpg',
+        thumbnail: '../images/Carlitos2.jpg',
+      },
+    ]
+ 
+    return (
+      <div className="col-5 image">
+      <ImageGallery items={images} showPlayButton={false}/>
+      </div>
+    );
+  }
+ 
 }
 
 export default SponsoredPhotos
