fe6139240ac6e8b8e0d4c49ac04696c06a3d028f
nosidam48 <madison.tift68@gmail.com>
2019-01-21 17:34:05 -0500

    basic schema and passport set-up (no function)

---
+++ models/content.js
@@ -0,0 +1,27 @@
+//Create the sequelize object for our Event table
+module.exports = function(sequelize, DataTypes) {
+  var content = sequelize.define("eventTable", {
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true
+      },
+      kid_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+      },
+      kid_notes: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      kid_pics: {
+          type: DataTypes.STRING,
+          allowNull: false
+      }
+  }, {
+      freezeTableName: true,
+      timestamps: false
+  });
+  return content;
+};
---
+++ models/kids.js
@@ -0,0 +1,48 @@
+//Create the sequelize object for our Event table
+module.exports = function(sequelize, DataTypes) {
+  var kids = sequelize.define("eventTable", {
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true
+      },
+      first_name: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      last_name: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      birth_date: {
+          type: DataTypes.DATE,
+          allowNull: false
+      },
+      grade: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      location: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      kid_bio: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      need_sponsor: {
+          type: DataTypes.BOOLEAN,
+          default: false,
+          allowNull: false
+      },
+      donor_id: {
+        type: DataTypes.INT,
+        allowNull: true
+      }
+  }, {
+      freezeTableName: true,
+      timestamps: false
+  });
+  return kids;
+};
