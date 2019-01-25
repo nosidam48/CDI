f909b1e03d27128b48fca21fff127b2389970fea
Ed Courtney <edcourtney74@gmail.com>
2019-01-25 12:44:15 -0500

    Added date to storing file name

--- routes/api/kids.js
+++ routes/api/kids.js
@@ -4,7 +4,7 @@ const storage = multer.diskStorage({
   destination: (req, file, cb)  => 
     cb(null, "./uploads/"),
   filename: (req, file, cb) => 
-    cb(null, file.originalname)
+    cb(null, file.originalname + '-' + Date.now())
 });
 
 const upload = multer({ storage: storage });
