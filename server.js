b16fff8a439b141fc26006fdb1e9cc470353e140
Ed Courtney <edcourtney74@gmail.com>
2019-01-30 11:53:52 -0500

    Sequelize error no longer happening

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ app.use(
 
 // Passport
 app.use(passport.initialize())
-app.use(passport.session()) // calls the deserializeUser
 
 // Send every request to the React app
 app.use('/user', user)
@@ -42,6 +41,7 @@ app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
+  app.use(passport.session()) // calls the deserializeUser
   app.use(express.static("client/build"));
   app.get("/*", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
