8537fe61aa36aec4e9d39058434d737b4b035f31
Ed Courtney <edcourtney74@gmail.com>
2019-02-14 12:54:18 -0500

    Displays message to user that profile was updated. Uses loading spinner too

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Row, Col, Form, Label } from "reactstrap";
 import { InputField, SubmitBtn } from "../components/Form";
 import MainContainer from "../components/Container";
-import axios from 'axios';
+import LoadingSpinner from "../components/LoadSpinner";
 import auth0Client from "../Auth";
 import API from "../utils/API";
 
@@ -17,7 +17,8 @@ class Profile extends Component {
       city: '',
       state: '',
       zip: '',
-      results: []
+      results: [],
+      loading: false
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
@@ -37,6 +38,8 @@ class Profile extends Component {
 
   handleSubmit(event) {
     event.preventDefault()
+    // Set loading state to true
+    this.setState({ loading: true})
     //Request to add/update user profile
     API.donorProfile({
       email: this.state.email,
@@ -49,7 +52,11 @@ class Profile extends Component {
     })
       .then(response => {
         console.log(response)
-        this.setState({results: response})
+        // Set state with db results and set loading to false
+        this.setState({ 
+          results: response.data,
+          loading: false
+        })
       }).catch(error => {
         console.log('Error updating the database')
         console.log(error)
@@ -59,67 +66,73 @@ class Profile extends Component {
   render() {
     return (
       <MainContainer>
+        {/* If results have come back, show success message. If not, show form */}
         {this.state.results.length > 0 ? (
           <h4 className="text-center mt-4">Your profile has been updated.</h4>
         ) : (
-          <Row>
-            <Col md={{ size: 6, offset: 3 }}>
-              <h4>Update Profile</h4>
-              <Form className="mt-3">
-                <Label>First Name*</Label>
-                <InputField
-                  value={this.state.firstName}
-                  onChange={this.handleChange}
-                  name="firstName"
-                  placeholder="Joe"
-                />
-                <Label>Last Name*</Label>
-                <InputField
-                  value={this.state.lastName}
-                  onChange={this.handleChange}
-                  name="lastName"
-                  placeholder="Smith"
-                />
-                <Label>Street Address</Label>
-                <InputField
-                  value={this.state.address}
-                  onChange={this.handleChange}
-                  name="address"
-                  placeholder="123 Main St."
-                />
-                <Label>City</Label>
-                <InputField
-                  value={this.state.city}
-                  onChange={this.handleChange}
-                  name="city"
-                  placeholder="Miami"
-                />
-                <Label>State</Label>
-                <InputField
-                  value={this.state.state}
-                  onChange={this.handleChange}
-                  name="state"
-                  placeholder="FL"
-                />
-                <Label>Zip Code</Label>
-                <InputField
-                  value={this.state.zip}
-                  onChange={this.handleChange}
-                  name="zip"
-                  placeholder="33129"
-                />
+            <Row>
+              <Col md={{ size: 6, offset: 3 }}>
+                <h4>Update Profile</h4>
+                <Form className="mt-3">
+                  <Label>First Name*</Label>
+                  <InputField
+                    value={this.state.firstName}
+                    onChange={this.handleChange}
+                    name="firstName"
+                    placeholder="Joe"
+                  />
+                  <Label>Last Name*</Label>
+                  <InputField
+                    value={this.state.lastName}
+                    onChange={this.handleChange}
+                    name="lastName"
+                    placeholder="Smith"
+                  />
+                  <Label>Street Address</Label>
+                  <InputField
+                    value={this.state.address}
+                    onChange={this.handleChange}
+                    name="address"
+                    placeholder="123 Main St."
+                  />
+                  <Label>City</Label>
+                  <InputField
+                    value={this.state.city}
+                    onChange={this.handleChange}
+                    name="city"
+                    placeholder="Miami"
+                  />
+                  <Label>State</Label>
+                  <InputField
+                    value={this.state.state}
+                    onChange={this.handleChange}
+                    name="state"
+                    placeholder="FL"
+                  />
+                  <Label>Zip Code</Label>
+                  <InputField
+                    value={this.state.zip}
+                    onChange={this.handleChange}
+                    name="zip"
+                    placeholder="33129"
+                  />
 
-                {/* Display submit button once first name and last name have values */}
-                <SubmitBtn
-                  disabled={!(this.state.firstName && this.state.lastName)}
-                  onClick={this.handleSubmit}
-                />
-              </Form>
-            </Col>
-          </Row>
-        )}
-        </MainContainer>
-      )
+                  {/* Display submit button once first name and last name have values */}
+                  <SubmitBtn
+                    disabled={!(this.state.firstName && this.state.lastName)}
+                    onClick={this.handleSubmit}
+                  />
+                </Form>
+              </Col>
+            </Row>
+          )}
+        {/* Shows loading spinner if loading is true */}
+        {this.state.loading ? (
+          <LoadingSpinner className="kidsSpin" />
+        ) : null
+        }
+      </MainContainer>
+    )
   }
 }
 
