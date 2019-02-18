88e383d19710cf74e062221f77461de877912eb0
Ed Courtney <edcourtney74@gmail.com>
2019-02-18 11:01:06 -0500

    Added loading icon for view admins

--- client/src/components/ViewAdmins/index.js
+++ client/src/components/ViewAdmins/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Table } from "reactstrap";
 import API from "../../utils/API";
+import LoadingSpinner from "../LoadSpinner";
 
 // Displays table of kid results
 class ViewAdmins extends React.Component {
 
     state = {
-        admins: []
+        admins: [],
+        loading: false,
     }
 
     componentDidMount() {
@@ -14,44 +16,50 @@ class ViewAdmins extends React.Component {
     }
 
     loadAdmins = (res) => {
-    
-        API.viewAdmins()
-        .then(res => {
-            this.setState({
-                admins: res.data
-            })
-            
-        }) 
+        // Set loading to true so spinner displays
+        this.setState({ loading: true });
 
+        API.viewAdmins()
+            .then(res => {
+                this.setState({
+                    admins: res.data,
+                    loading: false
+                })
+            })
     }
 
     render() {
-    return (
-        <div>
-            <Table className="mt-3">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>First Names</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Master Level?</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.admins.map(admin => (
-                    <tr key={admin.id}>
-                        <th scope="row">1</th>
-                        <td>{admin.first_name}</td>
-                        <td>{admin.last_name}</td>
-                        <td>{admin.email}</td>
-                        <td>{admin.master_admin_status ? "Yes" : "No"}</td>
-                    </tr>
-                    ))}
-                </tbody>
-            </Table>
-        </div>
-    )};
+        return (
+            <div>
+                {/* Display loader until results come, then display results */}
+                {this.state.loading ? <LoadingSpinner className="kidsSpin" />
+                    : (
+                        <Table className="mt-3">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>First Names</th>
+                                    <th>Last Name</th>
+                                    <th>Email</th>
+                                    <th>Master Level?</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.admins.map(admin => (
+                                    <tr key={admin.id}>
+                                        <th scope="row">{admin.id}</th>
+                                        <td>{admin.first_name}</td>
+                                        <td>{admin.last_name}</td>
+                                        <td>{admin.email}</td>
+                                        <td>{admin.master_admin_status ? "Yes" : "No"}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    )}
+            </div>
+        )
+    };
 }
 
 export default ViewAdmins;
